31a35d0a0ef5907c4ae6992a0dae4441e07c3533
Danila Ermakov <danila.ermakov@gmail.com>
2018-06-27 06:55:59 -0400

    changes

---
+++ src/main/kotlin/org/zoolytic/plugin/TableModel.kt
@@ -0,0 +1,45 @@
+package org.zoolytic.plugin
+
+import com.intellij.openapi.diagnostic.Logger
+import java.util.*
+import javax.swing.table.DefaultTableModel
+import javax.swing.tree.DefaultMutableTreeNode
+
+class TableModel : DefaultTableModel() {
+    private val LOG = Logger.getInstance(this.javaClass)
+
+    fun init() {
+        addColumn("Property")
+        addColumn("value")
+        arrayOf("Path", "Data", "Size", "Version", "Mod Date", "Stat").forEach { addRow(arrayOf(it, "")) }
+    }
+
+    override fun isCellEditable(row: Int, column: Int): Boolean {
+        if (row == 1 && column == 1) {
+            return true
+        }
+        return false
+    }
+
+    fun updateDetails(node: DefaultMutableTreeNode) {
+        if (node is ZkTreeNode) {
+            val nodeData = node.getNodeData()
+            with(nodeData, {
+                setValueAt(getFullPath(), 0, 1)
+                LOG.info("reading data")
+                setValueAt(if (data == null) {""} else {String(data!!)}, 1, 1)
+                LOG.info("reading data2")
+                if (stat == null) {
+                    (2..5).forEach { setValueAt("", it, 1) }
+                } else {
+                    setValueAt(stat?.getDataLength(), 2, 1)
+                    setValueAt(stat?.getVersion(), 3, 1)
+                    setValueAt(Date(stat!!.getMtime()), 4, 1)
+                    setValueAt(stat.toString(), 5, 1)
+                }
+                LOG.info("reading data10")
+
+            })
+        }
+    }
+}
---
+++ src/main/kotlin/org/zoolytic/plugin/ZkTreeCellRenderer.kt
@@ -0,0 +1,33 @@
+package org.zoolytic.plugin
+
+import com.intellij.ui.ColoredTreeCellRenderer
+import com.intellij.ui.SimpleTextAttributes
+import sun.swing.DefaultLookup
+import javax.swing.JTree
+
+class ZkTreeCellRenderer() : ColoredTreeCellRenderer() {
+    val leafIcon = DefaultLookup.getIcon(this, ui, "Tree.leafIcon")
+    val openIcon = DefaultLookup.getIcon(this, ui, "Tree.openIcon")
+    val closedIcon = DefaultLookup.getIcon(this, ui, "Tree.closedIcon")
+
+    override fun customizeCellRenderer(tree: JTree, node: Any?, selected: Boolean, expanded: Boolean, leaf: Boolean, row: Int, hasFocus: Boolean) {
+        if (leaf) {
+            icon = leafIcon
+        } else if (expanded) {
+            icon = openIcon
+        } else {
+            icon = closedIcon
+        }
+        if (node is ZkTreeNode) {
+            if (node is ZkRootTreeNode) {
+                append(node.getNodeData().text, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+                append(" (" + (if (node.isConnected()) {"coonected"} else "disconnected") + ")", SimpleTextAttributes.GRAY_ITALIC_ATTRIBUTES)
+            } else {
+                append(node.getNodeData().text, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+                append(node.getNodeData().getSizeString(), SimpleTextAttributes.GRAY_ITALIC_ATTRIBUTES)
+            }
+        } else {
+            append(node.toString(), SimpleTextAttributes.REGULAR_ATTRIBUTES)
+        }
+    }
+}
