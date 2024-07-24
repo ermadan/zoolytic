46f8e0ca5113b2ab3422efe05df0b26fe34acfd5
Danila Ermakov <danila.ermakov@gmail.com>
2024-07-24 15:33:00 -0400

    upgrade to intellij 241

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "org.zoolytic"
+version = "0.6.2"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2024.1.4")
+    pluginName.set("Zoolytic")
+
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+dependencies {
+    implementation(libs.zookeeper)
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("241")
+        changeNotes.set("""
+      upgrade to 241 idea version
+      """)
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
